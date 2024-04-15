ffc8c674153ce1c1f8634e3f0aee508b2b2ea6bd
ndtien142 <ndtien142@gmail.com>
2024-04-15 10:13:35 +0700

    init project

---
+++ Thuan2180605993/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thuan2180605993.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
